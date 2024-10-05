183b51a9f6771ffa21a3ddc46c4003d72fcf74b9
Sumith <miniproject202425@gmail.com>
2024-10-05 15:17:05 +0530

    Added navbar with title

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -1,15 +1,28 @@
-<?php 
-    session_start();
+<?php
+session_start();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="admin_dash.css">
 </head>
+
 <body>
-    <h1>TEST</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="page_title">
+            <a class="navbar-title" href="#">Library Admin Page</a>
+        </div>
+    </nav>
+
+    <!-- Script -->
+     <script src="js/bootstrap.js"></script>
+
 </body>
+
 </html>
