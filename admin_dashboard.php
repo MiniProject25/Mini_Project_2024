7a1e18b9a2d3a8ee91da84d11bf489b192c0cf21
Sumith <miniproject202425@gmail.com>
2024-10-06 16:56:20 +0530

    Added Top and Side Navbar

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -8,20 +8,54 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="admin_dash.css">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/admin_dash.css" />
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="page_title">
-            <a class="navbar-title" href="#">Library Admin Page</a>
+    <!-- Navbar -->
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1 page-title ms-auto me-auto">Library Admin Page</span>
         </div>
     </nav>
 
+    <!-- Offcanvas Sidebar -->
+    <div class="offcanvas offcanvas-start bg-dark text-white" tabindex="-1" id="offcanvasExample"
+        aria-labelledby="offcanvasExampleLabel">
+        <!-- <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+        </div> -->
+        <div class="offcanvas-body p-0">
+            <div class="navbar-dark">
+                <ul class="navbar-nav">
+                    <li>
+                        <a href="#" class="nav-link px-3 active">Import</a>
+                        <hr>
+                        <a href="#" class="nav-link px-3 active">Add a Student</a>
+                        <hr>
+                        <a href="#" class="nav-link px-3 active">Remove a Student</a>
+                        <hr>
+                        <a href="#" class="nav-link px-3 active">Edit</a>
+                        <hr>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <main class="mt-3">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 fs-3 fw-bold">Dashboard</div>
+            </div>
+        </div>
+    </main>
+
     <!-- Script -->
-     <script src="js/bootstrap.js"></script>
+    <!-- <script src="js/bootstrap.min.js"></script> -->
+    <script src="js/bootstrap.bundle.min.js"></script>
 
 </body>
 
