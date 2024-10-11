ecdaccec25fbd060ce6b33c5c90f69c1dcb048ca
Sumith <miniproject202425@gmail.com>
2024-10-11 14:11:01 +0530

    Added Student dropdown

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library Management</title>
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="css/index.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
@@ -35,44 +36,39 @@
                         <div class="mb-3">
                             <label for="year" class="form-label">Year of Registration</label>
                             <select class="form-select" id="year" aria-label="Select Year">
-                            <option selected disabled>Select year</option>
-                            <option value="2024">2024</option>
-                            <option value="2023">2023</option>
-                            <option value="2022">2022</option>
-                            <option value="2021">2021</option>
-                        </select>
+                                <option selected disabled>Select year</option>
+                                <option value="2024">2024</option>
+                                <option value="2023">2023</option>
+                                <option value="2022">2022</option>
+                                <option value="2021">2021</option>
+                            </select>
                         </div>
                         <div class="mb-3">
                             <label for="branch" class="form-label">Branch</label>
-                            <select  class="form-select" id="branch" placeholder="Enter branch">
-                            <option selected disabled>Select branch</option>
-                            <option value="CSE">Computer Science Engineering (CSE)</option>
-                            <option value="CSBS">computer science and business studies</option>
-                            <option value="CSD">computer science and design</option>
-                            <option value="AIML">Artificial intelligence and machine learning </option> 
-                            <option value="ECE">Electronics and Communication Engineering (ECE)</option>
-                            <option value="MECH">Mechanical Engineering (MECH)</option>
-                            <option value="CIVIL">Civil Engineering (CIVIL)</option>
-                            <option value="EEE">Electrical and Electronics Engineering (EEE)</option>
-                        </select>
+                            <select class="form-select" id="branch" placeholder="Enter branch">
+                                <option selected disabled>Select branch</option>
+                            </select>
                         </div>
                         <div class="mb-3">
                             <label for="section" class="form-label">Section</label>
                             <select class="form-select" id="section" aria-label="Select Section">
-                            <option selected disabled>Select section</option>
-                            <option value="A">A</option>
-                            <option value="B">B</option>
-                            <option value="C">C</option>
-                            <option value="D">D</option>
-                            <option value="E">E</option>
-                            <option value="F">F</option>
-                            <option value="G">G</option>
-                            <option value="H">H</option>
-                        </select></div>
-                        <div class="mb-3">
-                        <!--We can do the same drop down option for name section but importing from some other file all names , and when i type sum it should display aa person with sum... -->
+                                <option selected disabled>Select section</option>
+                                <option value="A">A</option>
+                                <option value="B">B</option>
+                                <option value="C">C</option>
+                                <option value="D">D</option>
+                                <option value="E">E</option>
+                                <option value="F">F</option>
+                                <option value="G">G</option>
+                                <option value="H">H</option>
+                            </select>
+                        </div>
+                        <div class="mb-3" id="studentListContainer" style="display: none;">
+                            <!--We can do the same drop down option for name section but importing from some other file all names , and when i type sum it should display aa person with sum... -->
                             <label for="studentName" class="form-label">Student Name</label>
-                            <input type="text" class="form-control" id="name" placeholder="Enter name">
+                            <select class="form-select" id="studentName" name="studentName">
+                                <option selected disabled>Select student</option>
+                            </select>
                         </div>
                     </form>
                 </div>
@@ -85,9 +81,9 @@
     </div>
 
     </div>
-    <script src="script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
 
     <!-- <script src=""></script> -->
 </body>
