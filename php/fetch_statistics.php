544c77e9fd2be68c119554fdb8a7ef19590f547a
Sumith <miniproject202425@gmail.com>
2024-10-29 20:17:53 +0530

    Added branch/year wise statistic option

--- php/fetch_statistics.php
+++ php/fetch_statistics.php
@@ -7,45 +7,65 @@ $branch = $_POST['branch'];
 $cyear = $_POST['cyear'];
 $data = [];
 
-// if branch and year isn't mentioned
 // if all the options are set (dates, branch and year)
-if (isset($data_from) && isset($date_to) && isset($branch) && isset($cyear)) {
-    // echo '<script> alert(Hello); </script>';
-    $_SESSION['message'] = "Hello";
-    $query = "SELECT HOUR(TimeIn) as hour, COUNT(*) as student_count 
-          FROM history
-          WHERE (Date BETWEEN ? AND ?) AND (Branch = ?) AND (Cyear = ?) 
-          GROUP BY HOUR(TimeIn) 
-          ORDER BY hour";
+if (isset($date_from) && isset($date_to)) {
+    // if branch and cyear is provided
+    if (!empty($branch) && !empty($cyear)) {
+        $query = "
+            SELECT HOUR(TimeIn) AS hour, COUNT(*) AS student_count
+            FROM history
+            WHERE (Date BETWEEN ? AND ?) AND (Branch = ?) AND (Cyear = ?)
+            GROUP BY HOUR(TimeIn)
+            ORDER BY hour";
 
-    $stmt = $conn->prepare($query);
-    $stmt->bind_param('ss', $date_from, $date_to);
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('sssi', $date_from, $date_to, $branch, $cyear);
+
+    } else if(!empty($branch)) {
+        $query = "
+            SELECT HOUR(TimeIn) AS hour, COUNT(*) AS student_count
+            FROM history
+            WHERE (Date BETWEEN ? AND ?) AND (Branch = ?) 
+            GROUP BY HOUR(TimeIn)
+            ORDER BY hour";
+
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('sss', $date_from, $date_to, $branch);
+
+    } else if (!empty($cyear)) {
+        $query = "
+            SELECT HOUR(TimeIn) AS hour, COUNT(*) AS student_count
+            FROM history
+            WHERE (Date BETWEEN ? AND ?) AND (Cyear = ?)
+            GROUP BY HOUR(TimeIn)
+            ORDER BY hour";
+
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('ssi', $date_from, $date_to, $cyear);
+
+    }else {
+        // Query for all branches and years
+        $query = "
+            SELECT HOUR(TimeIn) AS hour, COUNT(*) AS student_count
+            FROM history
+            WHERE Date BETWEEN ? AND ?
+            GROUP BY HOUR(TimeIn)
+            ORDER BY hour";
+
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('ss', $date_from, $date_to);
+    }
+
     $stmt->execute();
     $result = $stmt->get_result();
-    
+
     while ($row = $result->fetch_assoc()) {
         $data[] = $row;
     }
-    
-} else if (isset($data_from) && isset($date_to)) { // totality of statistics from all branches and years
-    // echo '<script> alert(World); </script>';  
-    $_SESSION['message'] = "World";
-    $query = "SELECT HOUR(TimeIn) as hour, COUNT(*) as student_count 
-          FROM history
-          WHERE Date BETWEEN ? AND ? 
-          GROUP BY HOUR(TimeIn) 
-          ORDER BY hour";
 
-    $stmt = $conn->prepare($query);
-    $stmt->bind_param('ss', $date_from, $date_to);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    echo json_encode($data);
 
-    while ($row = $result->fetch_assoc()) {
-        $data[] = $row;
-    }
+} else {
+    echo json_encode(['error' => 'Please provide valid date range.']);
 }
-
-
-echo json_encode($data);
 ?>
