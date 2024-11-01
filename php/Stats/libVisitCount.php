eba775a3f115be1e2fc0f39a2a1de634590e9ec6
Sumith <miniproject202425@gmail.com>
2024-11-01 12:08:52 +0530

    added branch and year-wise filtering

--- php/Stats/libVisitCount.php
+++ php/Stats/libVisitCount.php
@@ -3,19 +3,58 @@ include '../db_connection.php';
 
 $date_from = $_POST['date_from'];
 $date_to = $_POST['date_to'];
+$branch = $_POST['branch'];
+$cyear = $_POST['cyear'];
 $data = [];
 
 // LIBRARY USAGE PER HOUR
 // if all the options are set (dates, branch and year)
 if (isset($date_from) && isset($date_to)) {
 
-    $query = 'SELECT Date, COUNT(*) as visit_count
+    if (!empty($branch) && !empty($cyear)) {
+        // from, to, branch, year
+        $query = 'SELECT Date, COUNT(*) AS visit_count
+            FROM history
+            WHERE (Date BETWEEN ? AND ?) AND (Branch = ?) AND (Cyear = ?)
+            GROUP BY Date
+            ORDER BY Date';
+
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('sssi', $date_from, $date_to, $branch, $cyear);
+
+    } else if (!empty($branch)) {
+        // from, to, branch, all
+        $query = 'SELECT Date, COUNT(*) AS visit_count
+            FROM history
+            WHERE (Date BETWEEN ? AND ?) AND (Branch = ?) 
+            GROUP BY Date
+            ORDER BY Date';
+
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('sss', $date_from, $date_to, $branch);
+
+    } else if (!empty($cyear)) {
+        // from, to, all, year
+        $query = 'SELECT Date, COUNT(*) AS visit_count
+            FROM history
+            WHERE (Date BETWEEN ? AND ?) AND (Cyear = ?)
+            GROUP BY Date
+            ORDER BY Date';
+
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('ssi', $date_from, $date_to, $cyear);
+
+    } else {
+        // from, to, all, all
+        $query = 'SELECT Date, COUNT(*) as visit_count
             FROM history
             WHERE Date BETWEEN ? AND ?
             GROUP BY Date
             ORDER BY Date';
-    $stmt = $conn->prepare($query);
-    $stmt->bind_param('ss', $date_from, $date_to);
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('ss', $date_from, $date_to);
+    }
+
     $stmt->execute();
     $result = $stmt->get_result();
 
